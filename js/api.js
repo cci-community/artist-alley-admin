02a281696247559b47b9b67518d4b2d2ef475ecc
cci-community <nabaneet.mondal@comicconindia.com>
2026-08-07 01:30:37 +0530

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 // Replace with your Apps Script Web App URL
-const API_URL='PASTE_GOOGLE_APPS_SCRIPT_WEBAPP_URL';
+const API_URL='https://script.google.com/macros/s/AKfycbyh-KP3cEE0sPOUXgjrJ2KWnVGbUXyeO8OCcEyRzAjETMRVTfP4boqPjBhs_fd05PYg/exec';
 async function getArtists(){
  const r=await fetch(API_URL+'?sheet=Artists');
  return await r.json();
